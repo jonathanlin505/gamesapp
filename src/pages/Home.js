8f973c8d227feec93244d0efd49d1c5fa447ed53
jonathanlin505 <jlin300@gmail.com>
2020-12-28 22:13:39 +0800

    9. Global Styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,9 +23,35 @@ const Home = () => {
 
     return (
         <GameList>
+            <h2>Popular Games</h2>
+            <Games>
+                {popular.map((game) => (
+                    <Game
+                        name={game.name}
+                        released={game.released}
+                        id={game.id}
+                        image={game.background_image}
+                        key={game.id}
+                    />
+                ))}
+            </Games>
+
             <h2>Upcoming Games</h2>
             <Games>
                 {upcoming.map((game) => (
+                    <Game
+                        name={game.name}
+                        released={game.released}
+                        id={game.id}
+                        image={game.background_image}
+                        key={game.id}
+                    />
+                ))}
+            </Games>
+
+            <h2>New Games</h2>
+            <Games>
+                {newGames.map((game) => (
                     <Game
                         name={game.name}
                         released={game.released}
