b70b6c886019cccd70d385f18868f1e8202e4a7e
jonathanlin505 <jlin300@gmail.com>
2020-12-28 16:57:00 +0800

    3. Redux Setup

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import gamesReducers from "./gamesReducer";
+
+const rootReducer = combineReducers({
+    games: gamesReducers,
+});
+
+export default rootReducer;
